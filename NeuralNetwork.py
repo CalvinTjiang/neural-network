13200a3d752ca7ae7227b35c350e633101d73779
Calvin Tjiang <calvin.tjiang@gmail.com>
2019-12-05 16:35:24 +1100

    Add comment and SWISH Aativation function

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,17 +1,59 @@
 import numpy as np
 import numpy.ma as ma
+
 class Layer:
+    ''' Class to represent a layer in a neural network
+    
+    Static Attributes:
+        SIGMOID (int): 0, Logistic (sigmoid) function
+        TANH (int): 1, Hyperbolic Tangent (tanh) function
+        RELU (int): 2, Rectified Linear Unit (ReLU) function
+        SWISH (int): 3, Self-Gated Activation (Swish) function
+        SOFTMAX (int): 4, Normalized Exponential (softmax) function
+
+    Attributes:
+        total_nodes (int): total node, have to be greater than 0
+        weight (numpy.ndarray): each node's weight, 
+            Shape: (self.total_nodes, previous layer's total_nodes + 1)
+            Default : None
+
+        activation (numpy.ndarray): last activation value of input data,
+            Shape: (total input data, self.total_nodes + 1)
+            Default : None
+
+        z (numpy.ndarray): linear combination of each node's weight and the last input data,
+            Shape: (total input data, self.total_nodes)
+            Default : None
+
+        error (numpy.ndarray): each node's error rate,
+            Shape: (total input data, self.total_nodes)
+            Default : None
+
+        derivative (numpy.ndarray): each node's derivative value
+            Shape: (total input data, self.total_nodes + 1)
+            Default : None
+
+        activation_type (int): type of activation function
+            Default : Layer.SIGMOID (0)
+
+    '''
     # List of activation function type
     SIGMOID = 0
     TANH = 1
     RELU = 2 
-    SOFTMAX = 3
+    SWISH = 3
+    SOFTMAX = 4
 
  
     def __init__(self, total_nodes):
+        ''' Class __init__ method
+
+        Arguments:
+            total_nodes (int): total node, have to be greater than 0
+        '''
         self.total_nodes = total_nodes
-        self.weight = None
-        self.activation = None
+        self.weight = None 
+        self.activation = None          
         self.z = None
         self.error = None
         self.derivative = None
@@ -19,19 +61,33 @@ class Layer:
 
 
     def activate(self, previous_layer_activation):
+        ''' Calculate the layer's activation value of input data
+        
+        - Calculate the linear combination and stored the result in self.z
+            layer's linear combination = previous layer's activation value @ layer's weight.T
+
+        - Calculate the activation value depend on type of activation function
+
+        Arguments:
+            previous_layer_activation (numpy.ndarray): previous layer's last activation value of input data,
+                Shape: (total input data, previous layer's total_nodes + 1)
+        '''
         # Calculate the linear function of the layer weight and previous layer activation
         # Transpose the result, so that it have the same shape with previous layer activation
         self.z = previous_layer_activation @ self.weight.T
 
         # Check activation function type
         if self.activation_type == Layer.SIGMOID:
-            self.activation = (1 / (1 + np.exp(-self.z)))
+            self.activation = 1 / (1 + np.exp(-self.z))
         
         elif self.activation_type == Layer.TANH:
             self.activation = np.tanh(self.z)
 
         elif self.activation_type == Layer.RELU:
             self.activation = np.where(self.z > 0, self.z, 0)
+
+        elif self.activation_type == Layer.SWISH:
+            self.activation = self.z / (1 + np.exp(-self.z))
             
         elif self.activation_type == Layer.SOFTMAX:
             exponent = np.exp(self.z)
@@ -45,6 +101,17 @@ class Layer:
 
 
     def calculate_error(self, next_layer_error, next_layer_weight):
+        ''' Calculate layer's error rate 
+
+        layer's error rate = (next layer's error rate @ next layer's weight[:, 1:]) * derivative of the activation function
+
+        Arguments:
+            next_layer_error (numpy.ndarray): previous layer's last activation value of input data,
+                Shape: (total input data, next layer's total_nodes)
+
+            next_layer_weight (numpy.ndarray): previous layer's last activation value of input data,
+                Shape: (total input data, next layer's total_nodes + 1)
+        '''
         # g is the derivative of the activation function
         g = None
         if self.activation_type == Layer.SIGMOID:
@@ -56,31 +123,74 @@ class Layer:
         elif self.activation_type == Layer.RELU:
             g = np.where(self.z > 0, self.z, 0)
             g = np.where(g <= 0, g, 1)
-            
+
+        elif self.activation_type == Layer.SWISH:
+            g = (1 + self.z - self.activation[:, 1:]) * (self.activation[:, 1:]/ self.z)
+
+        # Should only be used for the last layer
         elif self.activation_type == Layer.SOFTMAX:
             # Should be used for the last layer
             pass
 
-        # Calculate the error
-        next_layer_weight = next_layer_weight[:, 1:]
+        else:
+            raise ValueError(f'{self.activation_type} is not a valid activation_type')
 
-        self.error = (next_layer_error @ next_layer_weight) * g
+        # Calculate the error
+        self.error = (next_layer_error @ next_layer_weight[:, 1:]) * g
 
 
     def derive(self, previous_layer_activation, total_data, learning_rate):
+        ''' Calculate each node's derivative
+        
+        layer's derivative = (layer's error.T @ previous layer's activation value) / total_data
+        layer's weight = layer's weight - learning_rate * layer's derivative
+
+        Arguments:
+            previous_layer_activation (numpy.ndarray): previous layer's last activation value of input data,
+                Shape: (total input data, next layer's total_nodes)
+
+            total_data (int): total_data, have to be greater than 0
+            learning_rate (float): layer's learning rate, have to be greater or equal to zero
+        '''
         self.derivative = (self.error.T @ previous_layer_activation) / total_data
         self.weight = self.weight - learning_rate * self.derivative
 
 
 class NeuralNetwork:
+    ''' Class to represent a layer in a neural network
+
+    Attributes:
+        layers (list): list of all Layer object
+        learning_rate (float): learning rate
+            Default: 1e-3 (0.001)
+            
+        batch_size (int): size of data for each training
+            Default: 100
+
+        total_epochs (int): total number of entire dataset are being used for training
+            Default: 1
+    '''
+
     def __init__(self):
         self.layers = []
         self.learning_rate = 1e-3
-        self.batch_size = 0
-        self.total_epochs = 0
+        self.batch_size = 100
+        self.total_epochs = 1
 
 
     def forward_propagation(self, x):
+        ''' Apply forward propagation to calculate each layer's activation value
+
+        Arguments:
+            x (numpy.ndarray): input data,
+                Shape: (total input data, input layer's total_nodes + 1)
+        
+        Precondition:
+            Input layer and output layer.
+        '''
+        if len(self.layers) <= 1:
+            raise Exception("Need at least input layer and output layer")
+
         # Create a vector of ones
         ones = np.ones((x.shape[0], 1))
 
@@ -91,19 +201,60 @@ class NeuralNetwork:
 
 
     def back_propagation(self, y):
+        ''' Apply back propagation to calculate the error rate
+
+        Arguments:
+            y (numpy.ndarray): the correct output data,
+                Shape: (total input data, output layer's total_nodes)
+        
+        Precondition:
+            Apply forward propagation with the same dataset.
+            Input layer and output layer.
+        '''
+        if len(self.layers) <= 1:
+            raise Exception("Need at least input layer and output layer")
+
         self.layers[-1].error = self.layers[-1].activation[:, 1:] - y
         for i in range(len(self.layers) - 2, 0, -1):
             self.layers[i].calculate_error(self.layers[i + 1].error, self.layers[i + 1].weight)
 
 
     def partial_derivative(self, total_data):
+        ''' Apply partial derivative to decrease each layer's error rate
+
+        Arguments:
+            total_data (int): total input data
+        
+        Precondition:
+            Apply back propagation with the same dataset.
+            Input layer and output layer.
+        '''
+        if len(self.layers) <= 1:
+            raise Exception("Need at least input layer and output layer")
+
         for i in range(1, len(self.layers)):
             self.layers[i].derive(self.layers[i - 1].activation, total_data, self.learning_rate)
 
 
     def train(self, x, y):
+        ''' Train the neural network to fit the given data
+
+        Arguments:
+            x (numpy.ndarray): input data,
+                Shape: (total input data, input layer's total_nodes + 1)
+
+            y (numpy.ndarray): the correct output data,
+                Shape: (total input data, output layer's total_nodes)
+        
+        Precondition:
+            Input layer and output layer.
+        '''
         if self.batch_size <= 0:
-            self.batch_size = len(x)
+            raise ValueError("batch_size have to be greater than 0")
+        
+        if len(self.layers) <= 1:
+            raise Exception("Need at least input layer and output layer")
+
         for epoch in range(self.total_epochs):
             # initial start batch is 0
             start_batch = 0 
@@ -127,12 +278,33 @@ class NeuralNetwork:
                 end_batch += self.batch_size
 
 
-    def predict(self, x):
+    def predict(self, x) -> np.ndarray:
+        ''' Calculate the output layer prediction for input data
+
+        Arguments:
+            x (numpy.ndarray): input data,
+                Shape: (total input data, input layer's total_nodes + 1)
+        
+        Precondition:
+            Input layer and output layer.
+        '''
         self.forward_propagation(x)
         return self.layers[-1].activation[:, 1:]
 
 
-    def cost(self, x, y):
+    def cost(self, x, y) -> np.ndarray:
+        ''' Calculate the cost of output layer compare with correct data
+
+        Arguments:
+            x (numpy.ndarray): input data,
+                Shape: (total input data, input layer's total_nodes + 1)
+
+            y (numpy.ndarray): the correct output data,
+                Shape: (total input data, output layer's total_nodes)
+        
+        Precondition:
+            Input layer and output layer.
+        '''
         prediction = self.predict(x)
 
         # Calculate Difference for y[i] = 1
@@ -147,7 +319,22 @@ class NeuralNetwork:
         return np.sum(total_cost)
 
 
-    def add_layer(self, total_nodes, activation_type, epsilon=1):
+    def add_layer(self, total_nodes, activation_type, epsilon=1.0):
+        ''' Append a new layer object to this neural network
+
+        Arguments:
+            total_nodes (int): new layer's total nodes
+
+            activation_type (int): new layer's activation function type
+
+            epsilon (float): the lower and upper bound for new layer's weight
+        '''
+        if total_nodes <= 0:
+            raise ValueError("total_nodes have to be greater than 0")
+
+        if epsilon <= 0:
+            raise ValueError("epsilon have to be greater than 0")
+
         new_layer = Layer(total_nodes)
         if len(self.layers) > 0:        
             new_layer.weight = np.random.uniform(-epsilon, epsilon, (total_nodes, self.layers[-1].total_nodes + 1))
